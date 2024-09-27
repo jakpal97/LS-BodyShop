4b0ac93ed4aba5edf5bf8d187b7df317ffd787d8
Jakpal97 <jakub.palka97@gmail.comgit config --global user.name Jakub>
2024-09-27 09:36:54 +0200

    dist changes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	base: './',
+	build: {
+		outDir: 'dist', // Określa folder wyjściowy
+		rollupOptions: {
+			input: './index.html', // Określa plik wejściowy dla Vite
+		},
+	},
 })
